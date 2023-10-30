8e46ed5d1ee81da4b1a1370b07a6b656d06c237b
unknown <agiusangelo13@gmail.com>
2023-10-30 20:05:04 +0100

    création du projet et ajout des premiers fichier

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/*const btn = document.querySelector("button")
+const p = document.querySelector("p")
+let click = 0
+
+btn.addEventListener("click", () => {
+    
+    click = click + 1
+
+    console.log(click)
+    p.innerHTML = click
+})*/
